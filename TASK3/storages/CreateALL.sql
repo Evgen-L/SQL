eea723562e99fc6243f5e35a4098fbdc0fc46e79
Evgen-L <61348033+Evgen-L@users.noreply.github.com>
2021-02-28 17:31:38 +0300

    fixes

--- TASK3/storages/CreateALL.sql
+++ TASK3/storages/CreateALL.sql
@@ -4,67 +4,69 @@ USE Ikea;
 
 CREATE TABLE Company
 (
-	Id INT PRIMARY KEY,    /*IDENTITY(1,1)*/
-	Name VARCHAR(20),
-	Address VARCHAR(20),
-	Telephone VARCHAR(20)
+	id        INT PRIMARY KEY,    
+	name      VARCHAR(20) UNIQUE,
+	address   VARCHAR(20),
+	telephone VARCHAR(20) UNIQUE
 )
 
 CREATE TABLE Storage
 (
-	Id INT PRIMARY KEY,
-	Name VARCHAR(20),
-	Serial_number INT,
-	Address VARCHAR(20),
-	Telephone VARCHAR(20),
-	CompanyId INT,
-	FOREIGN KEY (CompanyId)  REFERENCES Company(Id)
+	id            INT PRIMARY KEY,
+	name          VARCHAR(20),
+	serial_number INT,
+	address       VARCHAR(20),
+	telephone     VARCHAR(20),
+	id_company    INT,
+	UNIQUE(name, id_company),
+	FOREIGN KEY (id_company)  REFERENCES Company(id)
 )
 
 CREATE TABLE  Product
 (
-	Id INT IDENTITY,
-	Name VARCHAR(20) UNIQUE,
-	Price VARCHAR(10),
-	Quantity INT, 
-	PRIMARY KEY(Id)
+	id               INT PRIMARY KEY,
+	name             VARCHAR(20) UNIQUE,
+	price            VARCHAR(10),
+	type             VARCHAR(15),
+	date_manufacture DATE
 ); 
 
 CREATE TABLE  Product_in_storage
 (
-    Id INT PRIMARY KEY IDENTITY,
-	Name VARCHAR(20) UNIQUE,
-    StorageId INT,
-	ProductId INT,
-    Type VARCHAR(20),
-    FOREIGN KEY (StorageId)  REFERENCES Storage (Id)
+    id           INT PRIMARY KEY,
+    id_product   INT UNIQUE,
+	id_storage   INT,
+	date_arrival DATE,
+    FOREIGN KEY (id_storage)  REFERENCES Storage (id),
+	FOREIGN KEY (id_product)  REFERENCES Product (id)
 );
 
-CREATE TABLE Supplement_Product_In_Storage
-(
-	Product_in_storageId INT UNIQUE,
-	ProductId INT UNIQUE,
-	FOREIGN KEY (Product_in_storageId)  REFERENCES Product_in_storage (Id),
-	FOREIGN KEY (ProductId)  REFERENCES Product (Id)
-);
 
 CREATE TABLE Client
 (
-	Id INT PRIMARY KEY,
-	Name VARCHAR(20),
-	Age INT,
-	Telephone VARCHAR(20),
-	ProductId INT
+	id        INT PRIMARY KEY,
+	name      VARCHAR(20),
+	age       INT,
+	telephone VARCHAR(20) UNIQUE
 );
+
+CREATE TABLE Order_history
+(
+    id                    INT PRIMARY KEY,
+	id_client             INT,
+	id_product_in_storage INT UNIQUE,
+	date_order            DATE,
+	FOREIGN KEY (id_client)              REFERENCES Client (id),
+	FOREIGN KEY (id_product_in_storage)  REFERENCES Product_in_storage (id),
+)
+
+
 
 CREATE TABLE Payment
 (
-  ClientId INT,
-  ProductId INT,
-  Type VARCHAR(10),
-  Payment_Amount INT,
-  FOREIGN KEY (ClientId)  REFERENCES Client (Id),
-  FOREIGN KEY (ProductId)  REFERENCES Product (Id),
+  id_order_history INT PRIMARY KEY REFERENCES Order_history (id),
+  type             VARCHAR(10),
+  payment_amount   INT
 );
 
 
